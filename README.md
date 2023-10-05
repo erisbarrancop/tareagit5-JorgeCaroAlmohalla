7d75529d07688b5b37f52e348bdd7f417b9126aa
JorgeCaroAlmohalla <146361508+JorgeCaroAlmohalla@users.noreply.github.com>
2023-10-05 17:36:20 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Tarea5-Pull-Request-
+Tarea por parejas para hacer Pull Request
---
+++ Tarea5.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @author Jorge
+ */
+public class pullRequestGitHub {
+
+    public static void main(String args[]) {
+        System.out.println("Tarea 5-Pull-Request-")
+    }
+}
+
+
+    
